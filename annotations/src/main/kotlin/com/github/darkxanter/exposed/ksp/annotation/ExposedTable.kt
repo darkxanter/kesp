1c5b67304e33c4d65a037447bc307a99db23ec8b
Sergey Shumov <sergey0001@gmail.com>
2023-01-09 00:08:01 +0300

    added annotation documentation

--- annotations/src/main/kotlin/com/github/darkxanter/exposed/ksp/annotation/ExposedTable.kt
+++ annotations/src/main/kotlin/com/github/darkxanter/exposed/ksp/annotation/ExposedTable.kt
@@ -1,9 +1,16 @@
 package com.github.darkxanter.exposed.ksp.annotation
 
+/**
+ * Specifies that code generation will be run for the table.
+ */
+@MustBeDocumented
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 public annotation class ExposedTable(
+    /** Generate DTO for table. Required for CRUD repository generation. */
     val models: Boolean = true,
+    /** Generate extension methods for table. */
     val tableFunctions: Boolean = true,
+    /** Generate CRUD repository. */
     val crudRepository: Boolean = true,
 )
